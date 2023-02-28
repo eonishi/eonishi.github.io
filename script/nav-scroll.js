1b8a8e83547d0a53faef0088ffc083fcb688a449
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2023-02-28 00:05:02 -0300

    nuevo diseño minimal

--- script/nav-scroll.js
+++ script/nav-scroll.js
@@ -1,13 +1,14 @@
 // Detecta el evento de scroll de la ventana
 window.addEventListener('scroll', function() {
     // Obtiene la distancia desde la parte superior de la ventana del navegador hasta el inicio de la barra de navegación
-    var navbarOffset = document.querySelector('nav').offsetTop;
+    var navbarOffset = document.querySelector('.navbar').offsetTop;
   
     // Si la distancia desde la parte superior de la ventana del navegador es menor o igual al desplazamiento actual de la ventana, agrega la clase CSS 'navbar-fixed' a la barra de navegación
     if (navbarOffset <= window.pageYOffset) {
-      document.querySelector('nav').classList.add('navbar-fixed');
+      document.querySelector('.navbar').classList.add('navbar-fixed');
     } else {
       // Si no, remueve la clase CSS 'navbar-fixed'
-      document.querySelector('nav').classList.remove('navbar-fixed');
+      document.querySelector('.navbar').classList.remove('navbar-fixed');
     }
   });
+  
